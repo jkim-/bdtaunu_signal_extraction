887edc23c56f76dc1df0f4de4c3a7ce736573b78
dchao <dchao@caltech.edu>
2016-07-22 17:44:58 -0700

    Removed test directory.

--- test/subsample.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-with open('test.small.csv', 'w') as fout:
-    with open('data/test.csv', 'r') as fin:
-        for line in fin:
-            if np.random.random() < 0.001:
-                fout.write(line)
