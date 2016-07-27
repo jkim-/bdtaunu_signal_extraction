9a1dfb22c60539c8c0823a20a6f67b2063e4a749
dchao <dchao@caltech.edu>
2016-07-26 17:20:38 -0700

    Changed the directory structure... again.

--- visualization/mc.default/batch_visualization_data.py
+++ visualization/mc.default/batch_visualization_data.py
@@ -4,8 +4,6 @@ import argparse
 import subprocess
 
 parser = argparse.ArgumentParser(description='Batch produce visualization data. ')
-parser.add_argument('dataset_folder', type=str, 
-                   help='The sub-directory containing all the cfg files. ')
 parser.add_argument('evttypes', type=int, nargs='+',
                    help='any combination of the numeric codes infixed in a cfg file. ')
 args = parser.parse_args()
@@ -24,17 +22,17 @@ for evttype in args.evttypes:
   print 'Producing individual component data for {0}{1}...'.format(fname_prefix, evttype)
 
   out_fname = '{0}{1}.out'.format(fname_prefix, evttype)
-  cfg_fname = args.dataset_folder + '/{0}{1}.cfg'.format(fname_prefix, evttype)
+  cfg_fname = '{0}{1}.cfg'.format(fname_prefix, evttype)
   with open(out_fname, 'w') as f: 
     subprocess.check_call(
-        ['./visualize_individual_components', cfg_fname], 
+        ['../visualize_individual_components', cfg_fname], 
          stdout=f)
 
 print 'Producing stacked component data...'
-cfg_fname = args.dataset_folder + '/stacked_components.cfg'
+cfg_fname = '/stacked_components.cfg'
 with open(out_fname, 'w') as f: 
     subprocess.check_call(
-        ['./visualize_stacked_components', cfg_fname], 
+        ['../visualize_stacked_components', cfg_fname], 
          stdout=f)
 
 print
