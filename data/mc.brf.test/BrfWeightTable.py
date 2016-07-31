5ffc8b1b29c65ce4553b97069bbf306ee30ec28b
dchao <dchao@caltech.edu>
2016-07-31 14:24:59 -0700

    BrfWeighTable now accepts files with exactly 4 columns.

--- data/mc.brf.test/BrfWeightTable.py
+++ data/mc.brf.test/BrfWeightTable.py
@@ -1,14 +1,16 @@
-# Class that reads in file containing mode string, weight, weight uncertainty.
+# Class that reads in file containing mode string, weight, 
+# weight uncertainty, and the amount of fluctuation
 #
 # Valid file format:
 # 1. No header. 
-# 2. By default, space, tab, or comma.
-# 3. Each line contains at least 1 column, and at most 3 columns. 
+# 2. Space separated. 
+# 3. Each line contains exactly 4 columns. 
 #    + Column 1: string. indicates the mode. 
 #    + Column 2: float. indicates the weight. 
-#                        defaults to 1.0 if not provided. 
 #    + Column 3: float. indicates the uncertainty on the weight. 
-#                        defaults to 0.0 if not provided. 
+#    + Column 4: float. the number of standard devations to 
+#                       change the weight by. 
+#                       + (-) for increase (decrease).
 class BrfWeightTable:
 
     def __init__(self, lookup_fname):
@@ -23,21 +25,15 @@ class BrfWeightTable:
                 # tokenize the line
                 colvals = line.strip().split(' ')
 
-                # decide the weights and their fluctuations depending on
-                # the number of columns present
-                ncols = len(colvals)
-                if ncols not in range(1, 4):
+                if len(colvals) != 4:
                     raise RuntimeError(
-                            'BrfWeightTable: ' + lookup_fname + 
-                            ' must contain at least 1 and at most 3' + 
-                            'columns. ')
+                            'BrfWeightTable: ' + lookup_fname +
+                            ' must contain exact 4 columns per line. ')
 
-                weight, fluc_weight = 1.0, 0.0
-                if ncols == 2:
-                    weight = float(colvals[1])
-                elif ncols == 3:
-                    weight = float(colvals[1])
-                    fluc_weight = float(colvals[2])
+                # convert the values
+                weight = float(colvals[1])
+                weight_std = float(colvals[2])
+                z_score = float(colvals[3])
 
                 # insert into the table
                 if self.table.has_key(colvals[0]):
@@ -45,22 +41,22 @@ class BrfWeightTable:
                               'BrfWeightTable: ' + colvals[0] + 
                               ' already exists. ')
 
-                self.table[colvals[0]] = (weight, fluc_weight)
+                self.table[colvals[0]] = (weight, weight_std, z_score)
 
-    # return the pair (weight, fluc_weight) for the given key
-    def get_weight_pair(self, key):
+    # return the pair (weight, weight_std, z_score) for the given key
+    def get_weight_raw(self, key):
         return self.table.get(key)
 
-    # return weight + fluc_factor * fluc_weight for the given key
-    def get_weight(self, key, fluc_factor=0.0):
+    # return weight + z_score * weight_std for the given key
+    def get_weight(self, key):
 
-        weight_pair = self.table.get(key)
+        weight_raw = self.table.get(key)
 
         # no correction for None. this can happen when the given mode key
         # used to to find a correction was intentionally ommited.
-        if weight_pair is None: return 1.0
+        if weight_raw is None: return 1.0
 
-        return weight_pair[0] + fluc_factor * weight_pair[1]
+        return weight_raw[0] + weight_raw[2] * weight_raw[1]
 
 
 if __name__ == '__main__':
@@ -72,7 +68,7 @@ if __name__ == '__main__':
 
     print 
 
-    print brf_table.get_weight_pair('Bp_Dstar0_e_nu')
-    print brf_table.get_weight_pair('nonexistent_key')
-    print brf_table.get_weight('Bp_Dstar0_e_nu', -1.0)
-    print brf_table.get_weight('nonexistent_key', -1.0)
+    print brf_table.get_weight_raw('Bp_Dstar0_e_nu')
+    print brf_table.get_weight_raw('nonexistent_key')
+    print brf_table.get_weight('Bp_Dstar0_e_nu')
+    print brf_table.get_weight('nonexistent_key')
--- data/mc.brf.test/convert.py
+++ data/mc.brf.test/convert.py
@@ -3,7 +3,6 @@ from BrfWeightTable import BrfWeightTable
 if __name__ == '__main__':
 
     brf_table = BrfWeightTable('test.dat')
-    fluc_scale = -1.0
 
     with open('evttype4.train.csv', 'w') as fout:
 
@@ -15,8 +14,8 @@ if __name__ == '__main__':
 
                 w = float(colvals[2])
 
-                b1_brf_w = brf_table.get_weight(colvals[3], fluc_scale)
-                b2_brf_w = brf_table.get_weight(colvals[4], fluc_scale)
+                b1_brf_w = brf_table.get_weight(colvals[3])
+                b2_brf_w = brf_table.get_weight(colvals[4])
 
                 w = w * b1_brf_w * b2_brf_w
 
